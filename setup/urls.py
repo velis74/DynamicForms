0545898535e5e4a5828911c9cc669b6144ecfe09
Jure Erznožnik <jure.erznoznik@gmail.com>
2018-07-30 16:13:12 +0200

    Add DRF documentation

--- setup/urls.py
+++ setup/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from rest_framework.documentation import include_docs_urls
 
 urlpatterns = [
     url(r'^', include('examples.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'^docs/', include_docs_urls(title='Dynamic forms REST documentation'))
 ]
