fbbfb9290c38e1755b07db250c408f54270a3b93
Jure Erznožnik <jure.erznoznik@gmail.com>
2018-08-30 07:52:37 +0200

    remove constant url definitions

--- examples/rest.py
+++ examples/rest.py
@@ -6,12 +6,6 @@ from .models import Validated
 
 
 # TODO: templates/examples/validated* je treba prenest v dynamicforms/templates (standardni templati morajo bit pokrit)
-# TODO: Vnos recordov narediti preko dialoga
-# TODO:   oziroma: programer sam se odloči točno kaj se zgodi, ko se klikne "add" ali "edit" - tudi nekaj povsem tretjega...
-# TODO: paginate list template (ne zriši vseh 1500 recordov takoj, ampak jih dinamično loadaj, ko user poscrolla dovolj daleč dol
-# TODO:   pagination lahko narediš na dva načina:
-# TODO:     1. lahko imaš nek element pod tabelo in ko ta element pade v view, poskušaš naložit dodatne recorde
-# TODO:     2. lahko pa enemu od recordov proti koncu tabele obesiš to funkcionalnost in bo začel nalagat, ko ON pride v view. tako imaš možnost, da bi bili novi podatki naloženi še preden jih user dejansko potrebuje
 
 
 class ValidatedSerializer(serializers.ModelSerializer):
@@ -45,7 +39,7 @@ class ValidatedViewSet(ModelViewSet):
     # renderer_classes = [TemplateHTMLRenderer, JSONRenderer]
     template_name = 'examples/validated.html'
     list_template_name = 'examples/validated_list.html'
-    template_context = dict(crud_form=True)
+    template_context = dict(crud_form=True, url_reverse='validated')
 
     queryset = Validated.objects.all()
     serializer_class = ValidatedSerializer
