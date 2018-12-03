d851df9026909219c1405aba0d500fa95f1d1ae7
Jure Erznožnik <jure.erznoznik@gmail.com>
2018-12-03 13:10:40 +0100

    Another "first" release

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('requirements.txt', 'r') as fh:
 
 setuptools.setup(
     name="DynamicForms",
-    version="0.1.1",
+    version="0.1.2",
     author="Jure Erznožnik",
     author_email="jure@velis.si",
     description="DynamicForms is a django library that gives you the power of dynamically-shown form fields, "
