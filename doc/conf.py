355b9568d349ddd1b06099b329dca791bde126f7
Jure Erznožnik <jure.erznoznik@gmail.com>
2018-12-14 11:55:07 +0100

    0.2 release

--- doc/conf.py
+++ doc/conf.py
@@ -31,9 +31,9 @@ copyright = '2018, Velis Ltd'
 author = 'Velis Ltd'
 
 # The short X.Y version
-version = '0.1'
+version = '0.2'
 # The full version, including alpha/beta/rc tags
-release = '0.1 dev'
+release = '0.2 dev'
 
 # -- General configuration ---------------------------------------------------
 
