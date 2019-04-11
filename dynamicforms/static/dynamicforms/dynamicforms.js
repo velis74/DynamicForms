92013a4f159a943b0c96ae58dc7fd17a5fff13f3
Jure Erznožnik <jure.erznoznik@gmail.com>
2019-04-11 16:26:58 +0200

    True function refresh type

--- dynamicforms/static/dynamicforms/dynamicforms.js
+++ dynamicforms/static/dynamicforms/dynamicforms.js
@@ -155,6 +155,8 @@ dynamicforms = {
       }
     } else if (refreshType == 'page') {
       window.location.reload(true);
+    } else if (typeof(refreshType) == 'function') {
+      refreshType();
     } else if (refreshType.indexOf('redirect:') !== -1) {
       window.location.href = refreshType.split(':').pop();
     } else if (refreshType == 'no refresh') {
