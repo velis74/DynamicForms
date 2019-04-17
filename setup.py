a21530506ae3efef2f562f8f903c1b1031698e36
Jure Erznožnik <jure.erznoznik@gmail.com>
2019-04-17 07:48:20 +0200

    release 0.6.2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('requirements.txt', 'r') as fh:
 
 setuptools.setup(
     name="DynamicForms",
-    version="0.6.1",
+    version="0.6.2",
     author="Jure Erznožnik",
     author_email="jure@velis.si",
     description="DynamicForms performs all the visualisation & data entry of your DRF Serializers & ViewSets and adds "
