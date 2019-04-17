b90f3b49f0662746066ffc6fe061de1f404c991a
Jure Erznožnik <jure.erznoznik@gmail.com>
2019-04-17 10:44:13 +0200

    #27 - Field mixin: field in form / list #95 - Hide (disable) fields in form --------------------------------------- fix for SUPPRESS fix for number input being too wide by default

--- dynamicforms/mixins.py
+++ dynamicforms/mixins.py
@@ -15,7 +15,7 @@ from .action import Actions
 
 
 class DisplayMode(IntEnum):
-    SUPPRESS = 0  # Field will be entirely suppressed. it will not render (not even to JSON) and will not parse for PUT
+    SUPPRESS = 1  # Field will be entirely suppressed. it will not render (not even to JSON) and will not parse for PUT
     HIDDEN = 5  # Field will render as <input type="hidden"> or <tr data-field_name>
     INVISIBLE = 8  # Field will render completely, but with display: none. Equal to setting its style = {display: none}
     FULL = 10  # Field will render completely
