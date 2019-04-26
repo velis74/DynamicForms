5ecbe06ff87695aef1a9204e1f720e741c297e45
Klemen Pukl <klemen.pukl@velis.si>
2019-04-26 16:08:15 +0200

    Redirect 401 to login page

--- dynamicforms/viewsets.py
+++ dynamicforms/viewsets.py
@@ -133,8 +133,9 @@ class TemplateRendererMixin():
                     else:
                         serializer.render_type = 'form'
                         serializer.data_template = serializer.template_name
-            elif res.status_code == status.HTTP_403_FORBIDDEN and self.render_type != 'dialog':
-                # TODO: We should show a message here that user is not authorized for this action
+            elif res.status_code in (status.HTTP_401_UNAUTHORIZED, status.HTTP_403_FORBIDDEN) and \
+                    self.render_type != 'dialog':
+                # TODO: We should show a message here that user is not authorized for this action (only for 403)
                 res = redirect_to_login(request.path_info + get_query_params())
         return res
 
