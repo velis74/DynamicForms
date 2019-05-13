e07d435c86be7158ce8995c4ace3161c19db0bed
kspruk <spruk.klemen@gmail.com>
2019-05-13 19:44:44 +0200

    help text form to private var

--- dynamicforms/mixins.py
+++ dynamicforms/mixins.py
@@ -252,7 +252,7 @@ class FieldHelpTextMixin(object):
         super().__init__(*args, **kwargs, help_text=help_text)
         self.help_text = help_text
         if help_text_form or not isinstance(help_text, dict):
-            self.help_text_form = help_text_form or self.help_text
+            self._help_text_form = help_text_form or self.help_text
 
     def get_help_text(self):
         return self._help_text
