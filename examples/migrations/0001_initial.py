ba19c8b667fccc31e89c486a1f176e5194781135
Klemen Pukl <klemen.pukl@gmail.com>
2020-01-14 14:11:25 +0100

    Tox tests

--- examples/migrations/0001_initial.py
+++ examples/migrations/0001_initial.py
@@ -10,12 +10,14 @@ from django.db import migrations, models
 
 def add_relation(apps, schema_editor):
     from ..models import Relation
+    print("add_relation")
     for i in range(1, 11):
         Relation.objects.create(name='Relation object %d' % i)
 
 
 def add_filter(apps, schema_editor):
     from ..models import Filter
+    print("add_filter")
 
     date_now = django.utils.timezone.now()
     char_field = ['abc', 'def', 'ghi', 'abcdef', 'abcghi', 'defghi', 'abcdefghi']
@@ -31,8 +33,10 @@ def add_filter(apps, schema_editor):
 
 def add_page_load(apps, schema_editor):
     from ..models import PageLoad
+    print("add_page_load")
     for i in range(1, 10000 + 1):
-        PageLoad.objects.create(description='Item %d' % i, choice=random.randint(1, 4))
+        pl = PageLoad.objects.create(description='Item %d' % i, choice=random.randint(1, 4))
+        print('created', pl)
 
 
 class Migration(migrations.Migration):
