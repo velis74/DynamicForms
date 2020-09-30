4a34f8874c1c1c9723947f69e627504601862b48
Uros Tement <ut@scantron.si>
2020-09-30 07:45:03 +0200

    Password field added setting password_field property

--- dynamicforms/mixins/password_field.py
+++ dynamicforms/mixins/password_field.py
@@ -2,9 +2,15 @@ class PasswordFieldMixin(object):
 
     def __init__(self, *args, password_field=False, **kwargs):
         super().__init__(*args, **kwargs)
+        self.password_field = password_field
 
-        self.password_field = password_field or False
-
-        if self.password_field:
+    def set_password_field(self, value):
+        self._password_field = value
+        if value:
             self.display_table = 'DisplayMode.SUPRESS'
             self.style.update(input_type='password')
+        else:
+            self.display_table = 'DisplayMode.FULL'
+            self.style.update(input_type='text')
+
+    password_field = property(lambda self: self._password_field, set_password_field)
