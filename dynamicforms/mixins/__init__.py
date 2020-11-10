bc0e099e9839e11e45b2649f2fd5b52bb0c26614
Uros Tement <ut@scantron.si>
2020-11-10 09:21:47 +0100

    Updated sort order

--- dynamicforms/mixins/__init__.py
+++ dynamicforms/mixins/__init__.py
@@ -3,6 +3,7 @@
 from .action import ActionMixin
 from .allow_tags import AllowTagsMixin
 from .boolean_field import BooleanFieldMixin
+from .enable_copy import EnableCopyMixin
 from .help_text import FieldHelpTextMixin
 from .implicit_hidden import HiddenFieldMixin
 from .implicit_natural_date import DateFieldMixin, DateTimeFieldMixin, TimeFieldMixin
@@ -12,4 +13,3 @@ from .password_field import PasswordFieldMixin
 from .related_field_ajax import RelatedFieldAJAXMixin
 from .render import DisplayMode, RenderMixin
 from .rtf_field import RTFFieldMixin
-from .enable_copy import EnableCopyMixin
