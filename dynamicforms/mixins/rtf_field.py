4eae4efcedab18ff853e678bc7859e7f5b74be81
Uros Tement <ut@scantron.si>
2020-12-15 08:44:14 +0100

    Show rtf field as text input in filter

--- dynamicforms/mixins/rtf_field.py
+++ dynamicforms/mixins/rtf_field.py
@@ -1,15 +1,6 @@
-class RTFFieldMixin:
-    def __init__(self, **kw):
-        kwargs = dict(
-            max_length=None,
-            min_length=None,
-        )
-        for k, v in filter(lambda t: next(iter(t), None) and not t[0].startswith(('__', 'self', 'kw')),
-                           locals().items()):
-            kwargs[k] = v
-        kwargs.update(kw)
-        if kwargs.get('style', None) is None:
-            kwargs['style'] = dict(
-                base_template='rtf_field.html',
-            )
-        super().__init__(**kwargs)
+class RTFFieldMixin(object):
+
+    def to_representation(self, instance, row_data=None):
+        if not self.parent.is_filter:
+            self.style.update({'base_template': 'rtf_field.html'})
+        return super().to_representation(instance, row_data)
