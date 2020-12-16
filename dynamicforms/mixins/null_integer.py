1ddb49092819875d6e4659dd8dd3d659e0955037
Uros Tement <ut@scantron.si>
2020-12-16 13:39:48 +0100

    Null integer mixin extended to null value mixin for additional fields

--- dynamicforms/mixins/null_integer.py
+++
@@ -1,6 +0,0 @@
-class NullIntegerMixin(object):
-    def to_internal_value(self, data):
-        if data == '' and self.allow_null:
-            self.validators = []
-            return None
-        return super().to_internal_value(data)
