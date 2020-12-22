abba26f506f1e31cba8682754b529c031bb53fe6
Uroš Tement <utement@users.noreply.github.com>
2020-12-22 10:03:17 +0100

    Added parameters for plain-text display of RTF Field in table view
    * Added parameters for parsing for RTF Field
    
    * Renamed parameters for parser
    
    * Parser improvements
    
    Co-authored-by: Uros Tement <ut@scantron.si>

--- dynamicforms/mixins/rtf_field.py
+++ dynamicforms/mixins/rtf_field.py
@@ -4,25 +4,31 @@ from re import sub
 
 class RTFFieldMixin(object):
 
+    def __init__(self, *args, show_as_plain_text=False, plain_text_lines=3, plain_text_line_length=40, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.parse = show_as_plain_text or False
+        self.max_lines = plain_text_lines
+        self.max_line_length = plain_text_line_length
+        self.parser = RTFFieldHTMLParser(max_lines=self.max_lines, max_line_length=self.max_line_length)
+
     def to_representation(self, instance, row_data=None):
         if not self.parent.is_filter:
             self.style.update({'base_template': 'rtf_field.html'})
-        if self.is_rendering_to_list and not self.parent.is_filter:
-            parser = RTFFieldHTMLParser()
-            parser.feed(instance)
-            instance = parser.to_string()
+        if self.is_rendering_to_list and not self.parent.is_filter and self.parse:
+            self.parser.feed(instance)
+            instance = self.parser.to_string()
+            self.parser.reset()
 
         return super().to_representation(instance, row_data)
 
 
 class RTFFieldHTMLParser(HTMLParser):
-    text = []
-    max_lines = 3
-    max_line_length = 40
-    curr_line = 1
 
-    def __init__(self):
+    def __init__(self, max_lines, max_line_length):
         self.text = []
+        self.curr_line = 1
+        self.max_lines = max_lines
+        self.max_line_length = max_line_length
         super(RTFFieldHTMLParser, self).__init__()
 
     def handle_data(self, data):
@@ -35,6 +41,11 @@ class RTFFieldHTMLParser(HTMLParser):
                 text += '<br/>'
             self.text.append(text)
         self.curr_line += 1
+
+    def reset(self):
+        self.text = []
+        self.curr_line = 1
+        super(RTFFieldHTMLParser, self).reset()
 
     def to_string(self):
         return ''.join(self.text).strip()
