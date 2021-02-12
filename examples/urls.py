993366df23e6b76cb4f31487117bbe50c1883f1f
Jure Erznožnik <jure@velis.si>
2021-02-12 15:53:52 +0100

    drop support for ancient djangos, drfs and pythons

--- examples/urls.py
+++ examples/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import include, url
+from rest_framework.documentation import include_docs_urls
 
 from examples.rest import router
 from .views import index
@@ -7,9 +8,5 @@ urlpatterns = [
     url(r'^$', index, name='index'),
     url(r'^', include(router.urls)),
     url(r'^dynamicforms/', include('dynamicforms.urls')),
+    url(r'^api-docs/', include_docs_urls(title='Example API documentation')),
 ]
-
-from rest_framework.documentation import include_docs_urls
-urlpatterns.append(
-    url(r'^api-docs/', include_docs_urls(title='Example API documentation'))
-)
