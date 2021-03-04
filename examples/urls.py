0e79f41ec8e3ad5543bee8a474b1cf3efd2c66c2
KlemenŠpruk <spruk.klemen@gmail.com>
2021-03-04 16:17:24 +0100

    vue table example

--- examples/urls.py
+++ examples/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import include, url
 from rest_framework.documentation import include_docs_urls
 
 from examples.rest import router
-from .views import index, view_mode
+from .views import index, view_mode, vue_table
 
 urlpatterns = [
     url(r'^$', index, name='index'),
@@ -10,4 +10,6 @@ urlpatterns = [
     url(r'^', include(router.urls)),
     url(r'^dynamicforms/', include('dynamicforms.urls')),
     url(r'^api-docs/', include_docs_urls(title='Example API documentation')),
+    url(r'^vue-table', vue_table, name='vue-table'),
+
 ]
