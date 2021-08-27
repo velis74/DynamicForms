4dbcc6084865fdf6cfef33f741f8de5870cec38d
Jure Erznožnik <jure@velis.si>
2021-08-27 15:08:28 +0200

    Support for additional command parameters

--- dynamicforms_dev/management/commands/npm_run.py
+++ dynamicforms_dev/management/commands/npm_run.py
@@ -1,6 +1,6 @@
+import argparse
 import os
 import subprocess
-from argparse import ArgumentParser
 
 from django.core.management.base import BaseCommand
 
@@ -8,13 +8,15 @@ from django.core.management.base import BaseCommand
 class Command(BaseCommand):
     help = 'Generate Field classes from DRF with applied DynamicForms mixins'
 
-    def add_arguments(self, parser: ArgumentParser):
+    def add_arguments(self, parser: argparse.ArgumentParser):
         parser.add_argument('command', type=str, default='serve', action='store', nargs='?',
                             help='command to run, e.g. "build" or "serve". default: "serve"')
+        parser.add_argument('cmd_params', nargs=argparse.REMAINDER)
 
     def handle(self, *args, **options):
         cmd = options.get('command')
         pth = os.path.abspath(os.path.join(os.path.split(__file__)[0], '..', '..', '..'))
         pth = os.path.join(pth, 'dynamicforms', 'df-vue-components')
-        p = subprocess.Popen(['npm', 'run', cmd], cwd=pth)
+        print(['npm', 'run', cmd] + options.get('cmd_params'))
+        p = subprocess.Popen(['npm', 'run', cmd] + options.get('cmd_params'), cwd=pth)
         p.wait()
