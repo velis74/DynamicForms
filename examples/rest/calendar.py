dad90ad3e0a34c494ae62bedac46a8e70648c115
Jure Erznožnik <jure@velis.si>
2021-11-18 06:34:11 +0100

    #656 - Calendar sample: API for recurrence #657 - Calendar sample: API for recurrence unit tests

--- examples/rest/calendar.py
+++ examples/rest/calendar.py
@@ -1,8 +1,8 @@
-from dynamicforms import serializers, viewsets
 from examples.models import CalendarEvent
+from .calendar_recurrence import RecurrenceEventSerializer, RecurrenceEventViewSet, RecurrenceSerializer
 
 
-class CalendarEventSerializer(serializers.ModelSerializer):
+class CalendarEventSerializer(RecurrenceEventSerializer):
     template_context = dict(url_reverse='calendar-event')
     form_titles = {
         'table': 'Calendar',
@@ -10,11 +10,13 @@ class CalendarEventSerializer(serializers.ModelSerializer):
         'edit': 'Editing event',
     }
 
+    recurrence = RecurrenceSerializer(required=False)
+
     class Meta:
         model = CalendarEvent
         exclude = ()
 
 
-class CalendarEventViewSet(viewsets.ModelViewSet):
+class CalendarEventViewSet(RecurrenceEventViewSet):
     serializer_class = CalendarEventSerializer
     queryset = CalendarEvent.objects.all()
