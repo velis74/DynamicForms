eadf541a13e2a3c1eca970412ec3369641e3bd31
Jure Erznožnik <jure@velis.si>
2021-12-13 15:18:31 +0100

    ActionHandlerMixin refactor action methods to single method for single purpose

--- dynamicforms/vue/src/mixins/actionHandlerMixin.js
+++ dynamicforms/vue/src/mixins/actionHandlerMixin.js
@@ -20,7 +20,9 @@ const callDbFunction = (payload) => {
   let params;
   if (['put', 'post', 'patch'].includes(payload.method)) {
     params = [payload.url, payload.data, payload.config];
-  } else params = [payload.url, payload.config];
+  } else {
+    params = [payload.url, payload.config];
+  }
 
   apiClient[payload.method].apply(this, params)
     .then((res) => { thenFunction(res); })
@@ -37,9 +39,28 @@ const actionHandlerMixin = {
   },
   methods: {
     tableAction(payload) {
-      if (['add', 'edit', 'delete', 'filter', 'submit', 'cancel'].includes(payload.action.name)) {
-        this.executeTableAction(payload.action, payload.data, payload.modal, {});
-      } else {
+      if (payload.action === undefined || !_.size(payload.action)) return;
+
+      switch (payload.action.name) {
+      case 'add':
+        this.actionAddExecute(payload.action, payload.data, payload.modal, {});
+        break;
+      case 'edit':
+        this.actionEditExecute(payload.action, payload.data, payload.modal, {});
+        break;
+      case 'delete':
+        this.actionDeleteExecute(payload.action, payload.data, payload.modal, {});
+        break;
+      case 'filter':
+        this.actionFilterExecute(payload.action, payload.data, payload.modal, {});
+        break;
+      case 'submit':
+        this.actionSubmitExecute(payload.action, payload.data, payload.modal, {});
+        break;
+      case 'cancel':
+        this.actionCancelExecute(payload.action, payload.data, payload.modal, {});
+        break;
+      default: {
         const action = payload.action ? payload.action.action : null;
         if (action === null) return; // Action is empty or not there, nothing will be executed
 
@@ -64,71 +85,74 @@ const actionHandlerMixin = {
           }
         }
       }
+      }
     },
-    executeTableAction(action, data, modal, params) {
-      if (!_.size(action)) {
-        return;
+    actionAddExecute(action, data, modal, params) { // eslint-disable-line no-unused-vars
+      this.showModal(action, data);
+    },
+    actionEditExecute(action, data, modal, params) { // eslint-disable-line no-unused-vars
+      this.showModal(action, data);
+    },
+    actionDeleteExecute(action, data, modal, params) {
+      const url = params && params.detailUrl ? params.detailUrl : this.detail_url.replace('--record_id--', data.id);
+      callDbFunction({
+        method: 'delete',
+        url,
+        then: params.then ||
+          (() => {
+            if (this && this.processedConfiguration) this.processedConfiguration.rows.deleteRow(data.id);
+          }),
+      });
+    },
+    actionSubmitExecute(action, data, modal, params) {
+      let dataId;
+      let submitMethod;
+      if (data.id) {
+        dataId = data.id;
+        submitMethod = 'put';
+      } else {
+        dataId = 'new';
+        submitMethod = 'post';
       }
-      if (action.name === 'delete') {
-        const url = params && params.detailUrl ? params.detailUrl : this.detail_url.replace('--record_id--', data.id);
-        callDbFunction({
-          method: 'delete',
-          url,
-          then: params.then ||
-            (() => {
-              if (this && this.processedConfiguration) this.processedConfiguration.rows.deleteRow(data.id);
-            }),
-        });
-      } else if (['edit', 'add'].includes(action.name)) {
-        this.showModal(action, data);
-      } else if (action.name === 'submit') {
-        let dataId;
-        let submitMethod;
-        if (data.id) {
-          dataId = data.id;
-          submitMethod = 'put';
-        } else {
-          dataId = 'new';
-          submitMethod = 'post';
-        }
-        submitMethod = params.submitMethod || submitMethod;
+      submitMethod = params.submitMethod || submitMethod;
 
-        // noinspection JSUnresolvedVariable
-        const url = params && params.detailUrl ? params.detailUrl : this.detail_url.replace('--record_id--', dataId);
-        const headers = params && 'headers' in params ?
-          params.headers : { 'x-viewmode': 'TABLE_ROW', 'x-pagination': 1, 'x-df-component-def': true };
-        const self = this;
+      // noinspection JSUnresolvedVariable
+      const url = params && params.detailUrl ? params.detailUrl : this.detail_url.replace('--record_id--', dataId);
+      const headers = params && 'headers' in params ?
+        params.headers : { 'x-viewmode': 'TABLE_ROW', 'x-pagination': 1, 'x-df-component-def': true };
+      const self = this;
 
-        callDbFunction({
-          method: submitMethod,
-          url,
-          data,
-          config: { headers },
-          then: params.then ||
-            ((res) => {
-              if (modal) modal.hide();
-              if (self && self.processedConfiguration) self.processedConfiguration.rows.updateRowFromForm(res.data);
-            }),
-          catch: params.catch ||
-            ((reason) => {
-              const dfErrors = {};
-              const eventName = `formEvents_${modal.currentDialog.body.uuid}`;
-              if (reason.response.status === 400) {
-                _.forOwn(reason.response.data, (value, key) => {
-                  dfErrors[`${key}`] = _.join(value, '\n');
-                });
-              } else {
-                dfErrors.non_field_errors = reason.response.data.detail;
-              }
+      callDbFunction({
+        method: submitMethod,
+        url,
+        data,
+        config: { headers },
+        then: params.then ||
+          ((res) => {
+            if (modal) modal.hide();
+            if (self && self.processedConfiguration) self.processedConfiguration.rows.updateRowFromForm(res.data);
+          }),
+        catch: params.catch ||
+          ((reason) => {
+            const dfErrors = {};
+            const eventName = `formEvents_${modal.currentDialog.body.uuid}`;
+            if (reason.response.status === 400) {
+              _.forOwn(reason.response.data, (value, key) => {
+                dfErrors[`${key}`] = _.join(value, '\n');
+              });
+            } else {
+              dfErrors.non_field_errors = reason.response.data.detail;
+            }
 
-              eventBus.$emit(eventName, { type: 'submitErrors', data: dfErrors });
-            }),
-        });
-      } else if (action.name === 'cancel') {
-        if (modal) modal.hide();
-      } else if (action.name === 'filter') {
-        this.loadData();
-      }
+            eventBus.$emit(eventName, { type: 'submitErrors', data: dfErrors });
+          }),
+      });
+    },
+    actionCancelExecute(action, data, modal, params) { // eslint-disable-line no-unused-vars
+      if (modal) modal.hide();
+    },
+    actionFilterExecute(action, data, modal, params) { // eslint-disable-line no-unused-vars
+      this.loadData();
     },
   },
 };
