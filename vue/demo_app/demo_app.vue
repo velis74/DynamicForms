479441aad1e8a31bf0de1814bc62258d9b215fa5
KlemenŠpruk <spruk.klemen@gmail.com>
2022-03-18 09:55:51 +0100

    reset resources on page load if needed

--- vue/demo_app/demo_app.vue
+++ vue/demo_app/demo_app.vue
@@ -82,30 +82,47 @@ export default {
       }
     },
   },
+  mounted() {
+    this.resetResources(null);
+  },
   methods: {
     setTitle(newTitle) {
       this.title = newTitle;
       document.title = `${newTitle} - DynamicForms`;
     },
+    removeResources(theme) {
+      const removed = [];
+      _.forEach(Array.prototype.slice.call(document.getElementsByTagName('link')).concat(
+        Array.prototype.slice.call(document.getElementsByTagName('script')),
+      ), (s) => {
+        if ((s.href && _.includes(s.href, `.${theme}.css`)) ||
+              (s.src && _.includes(s.src, `.${theme}.js`)) ||
+              (s.href && _.includes(s.href, `.${theme}.js`))) {
+          document.getElementsByTagName('head')[0].removeChild(s);
+          removed.push(s);
+        }
+      });
+      return removed;
+    },
+    resetResources(newTheme) {
+      if (!newTheme) {
+        _.forEach(_.filter(this.themes, (t) => t !== this.theme), (th) => {
+          this.removeResources(th);
+        });
+        return;
+      }
+      const removedResources = this.removeResources(this.theme);
+      if (!_.size(this.themeData[this.theme])) {
+        this.themeData[this.theme] = this.themeData[this.theme].concat(removedResources);
+      }
+      _.forEach(this.themeData[newTheme], (v) => {
+        document.getElementsByTagName('head')[0].appendChild(v);
+      });
+    },
     changeTheme(newTheme) {
       localStorage.setItem('df-theme', newTheme);
       if (newTheme !== this.theme) {
-        const saveOldData = !_.size(this.themeData[this.theme]);
-        _.forEach(Array.prototype.slice.call(document.getElementsByTagName('link')).concat(
-          Array.prototype.slice.call(document.getElementsByTagName('script')),
-        ), (s) => {
-          if ((s.href && _.includes(s.href, `.${this.theme}.css`)) ||
-              (s.src && _.includes(s.src, `.${this.theme}.js`)) ||
-              (s.href && _.includes(s.href, `.${this.theme}.js`))) {
-            document.getElementsByTagName('head')[0].removeChild(s);
-            if (saveOldData) {
-              this.themeData[this.theme].push(s);
-            }
-          }
-        });
-        _.forEach(this.themeData[newTheme], (v) => {
-          document.getElementsByTagName('head')[0].appendChild(v);
-        });
+        this.resetResources(newTheme);
         this.theme = newTheme;
       }
     },
