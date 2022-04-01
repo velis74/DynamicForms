29d9f312b7b280fc3815492e06b79ea4adde0555
Klemen Pukl <brontes>
2022-04-01 12:48:13 +0200

    #717 - Watching entire payload record doesn't work as expected

--- vue/demo_app/example_hidden_layout.vue
+++ vue/demo_app/example_hidden_layout.vue
@@ -17,7 +17,11 @@ export default {
     return { oldPayload: this.payload.deepClone() };
   },
   computed: { // eslint-disable-line object-curly-newline
-    unitVisible() { return this.layout.fields.unit.visibility === DisplayMode.FULL; },
+    unitVisible() {
+      // TODO: Looks like layout.fields.unit.visibility is not reactive... It doesn't update when you call setVisibility
+      //  hence method unitVisibleMethod().
+      return this.layout.fields.unit.visibility === DisplayMode.FULL;
+    },
   },
   watch: { // eslint-disable-line object-curly-newline
     // Watching variables is one way of catering for dynamic field visibility / initialisation
@@ -29,7 +33,7 @@ export default {
       handler(newValue) {
         // See note in handler funcion on https://vuejs.org/guide/essentials/watchers.html#deep-watchers
         // If we want to track changes we have to manually keep previous value in custom variable.
-        this.unitChanged(newValue);
+        this.unitChanged(newValue.unit);
         this.oldPayload = newValue.deepClone();
       },
       deep: true,
@@ -38,6 +42,9 @@ export default {
   mounted() { this.unitChanged(this.payload); },
   updated() { this.unitChanged(this.payload); },
   methods: {
+    unitVisibleMethod() {
+      return this.layout.fields.unit.visibility === DisplayMode.FULL;
+    },
     valueChanged(payload) {
       // Tracking value-changed events emitted by the Layout is one of two ways of catering for dynamic field visibility
       //   Watching variables is another. See watch handler above
@@ -48,9 +55,25 @@ export default {
       this.unitChanged(this.payload.unit);
     },
     unitChanged(newValue) {
-      this.layout.fields.int_fld.setVisibility(this.unitVisible && ['pcs', 'cst'].includes(newValue));
-      this.layout.fields.qty_fld.setVisibility(this.unitVisible && newValue === 'wt');
-      this.layout.fields.cst_fld.setVisibility(this.unitVisible && newValue === 'cst');
+      let intVisibility = this.unitVisibleMethod();
+      let qtyVisibility = this.unitVisibleMethod();
+      let cstVisibility = this.unitVisibleMethod();
+      if (this.unitVisibleMethod()) {
+        intVisibility = ['pcs', 'cst'].includes(newValue);
+        qtyVisibility = newValue === 'wt';
+        cstVisibility = newValue === 'cst';
+      }
+
+      // If you call setVisibility on field you are currently on it looses focus. Even if visibility didn't change.
+      if (this.layout.fields.int_fld.isVisible !== intVisibility) {
+        this.layout.fields.int_fld.setVisibility(intVisibility);
+      }
+      if (this.layout.fields.qty_fld.isVisible !== qtyVisibility) {
+        this.layout.fields.qty_fld.setVisibility(qtyVisibility);
+      }
+      if (this.layout.fields.cst_fld.isVisible !== cstVisibility) {
+        this.layout.fields.cst_fld.setVisibility(cstVisibility);
+      }
     },
   },
 };
