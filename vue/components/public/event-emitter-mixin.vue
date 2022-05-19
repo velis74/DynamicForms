15f3c149d940d7743ac3db0889fe5572c2b1be01
Jure Erznožnik <jure@velis.si>
2022-05-19 15:27:33 +0200

    #727 - Common identifier for top-level components that emit events

---
+++ vue/components/public/event-emitter-mixin.vue
@@ -0,0 +1,31 @@
+<script>
+import _ from 'lodash';
+
+export default {
+  name: 'EventEmitterMixin',
+  data() { return { dfEventEmitter: false }; },
+  computed: {
+    parentEmitter() {
+      let parent = this.$parent;
+      while (parent && parent.dfEventEmitter !== true) parent = parent.$parent;
+      return parent;
+    },
+  },
+  methods: {
+    emit(eventName, eventData) {
+      // If this component is an event emitter, emit. Also emit if this component has no parent that is an emitter
+      if (this.dfEventEmitter || !this.parentEmitter) this.$emit(eventName, eventData);
+      if (this.parentEmitter) {
+        // if there's yet a parent that is an emitter, have it emit the event as well
+        const oldValue = _.cloneDeep(this.payload);
+        oldValue[eventData.field] = eventData.oldValue;
+        console.log('a', { field: eventData.field.name, oldValue, newValue: this.payload });
+        this.parentEmitter.emit(
+          'value-changed',
+          { field: this.parentEmitter.fieldName, oldValue, newValue: this.payload },
+        );
+      }
+    },
+  },
+};
+</script>
