d03c451c9349332ec8c5433e2fb061a376316ad5
Jure Erznožnik <jure@velis.si>
2022-05-21 09:58:31 +0200

    #704 - Create bootstrap form components (partial implementation)

--- vue/components/vuetify.js
+++ vue/components/vuetify.js
@@ -1,7 +1,7 @@
 import VuetifyActions from './actions/actions_vuetify';
 import VuetifyForm from './form/form_vuetify';
-import VuetifyFormLayout from './form/layout_vuetify';
+import FormLayout from './form/layout';
 import VuetifyModal from './modal/modal_api_vuetify';
 import VuetifyTable from './table/table_vuetify';
 
-export { VuetifyTable, VuetifyForm, VuetifyFormLayout, VuetifyActions, VuetifyModal };
+export { FormLayout, VuetifyTable, VuetifyForm, VuetifyActions, VuetifyModal };
