c085d2de71eeb73a8b0a9c85561e05e09c7057f6
KlemenSpruk <spruk.klemen@gmail.com>
2022-06-02 09:54:55 +0200

    #728 Actions: emit events to top-level parent POPRAVKI (#122)
    * saving
    
    * added table delete action
    
    * pr fix
    
    * pr fix
    
    * pr fixes

--- vue/components/public/event-emitter-mixin.vue
+++ vue/components/public/event-emitter-mixin.vue
@@ -4,19 +4,16 @@ import _ from 'lodash';
 export default {
   name: 'EventEmitterMixin',
   methods: {
-    getHandler(functionName, getPayload = false) {
+    getHandlerWithPayload(functionName) {
       let parent = this.$parent;
       let payload = null;
       while (parent && parent[functionName] === undefined) {
         parent = parent.$parent;
-        if (parent && parent.payload !== undefined && !payload) {
+        if (parent && parent.payload !== undefined) {
           payload = parent.payload;
         }
       }
-      if (getPayload) {
-        return payload;
-      }
-      return parent;
+      return [parent, payload];
     },
     getDispatchActionFunctionName(actionData) {
       return `action${_.startCase(_.toLower(actionData.name))}`;
@@ -26,17 +23,18 @@ export default {
        * Action is handled by a specific function on a component. First parent component which has this function
        * defined executes this function with arguments actionData, payload. Payload is a computed variable, for payload
        * value, value of first parent component's payload computed value is taken if payload computed var is defined.
+       * If this function returns false then current component tries to exceute functionName(payload)
        *
        * Function name which executes action data is "calculated from actionData attributes."
        */
 
       const functionName = this.getDispatchActionFunctionName(actionData);
-      const handler = this.getHandler(functionName);
-      if (handler && handler[functionName]) {
-        handler[functionName](actionData, this.getHandler(functionName, true));
-        return;
+      const handlerAndPayload = this.getHandlerWithPayload(functionName);
+      let payloadProcessedByHandler = false;
+      if (handlerAndPayload[0] && handlerAndPayload[0][functionName]) {
+        payloadProcessedByHandler = handlerAndPayload[0][functionName](actionData, handlerAndPayload[1]);
       }
-      if (this[functionName]) {
+      if (!payloadProcessedByHandler && this[functionName]) {
         this[functionName](actionData, this.payload);
       }
     },
