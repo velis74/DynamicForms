7d375eac23ec4e57928c0c581647ecf10557bff2
adamPrestor <adam.prestor@gmail.com>
2022-11-18 10:56:56 +0100

    #800 Intermediate port, still missing few css and js functionalities + vitest

---
+++ vite.config.js
@@ -0,0 +1,74 @@
+import { defineConfig } from 'vite';
+import { createVuePlugin as vue2 } from 'vite-plugin-vue2';
+import Components from 'unplugin-vue-components/vite';
+import { VuetifyResolver } from 'unplugin-vue-components/resolvers';
+import eslint from 'vite-plugin-eslint';
+
+const { createProxyMiddleware } = require('http-proxy-middleware');
+const axiosRedirectConfig = () => ({
+  name: 'serverProxy',
+  configureServer(server) {
+    const filter = function (pathname, req) {
+      return typeof req.headers['x-csrftoken'] != 'undefined'
+    }
+    server.middlewares.use(
+      '/',
+      createProxyMiddleware(filter, {
+        target: 'http://localhost:8000',
+        changeOrigin: false,
+        pathRewrite: path => {
+          console.log('path', path);
+          return path;
+        }
+      })
+    )
+  }
+});
+
+/** @type {import('vite').UserConfig} */
+export default defineConfig({
+  plugins: [
+    vue2({
+      jsx: true,
+    }),
+    {
+      ...eslint({
+        failOnWarning: false,
+        failOnError: false
+      }),
+      apply: 'serve',
+      enforce: 'post'
+    },
+    Components({
+      resolvers: [VuetifyResolver()]
+    }),
+    axiosRedirectConfig()
+  ],
+  resolve: {
+    alias: [
+      {
+        find: /^~(.*)$/,
+        replacement: 'node_modules/$1',
+      },
+    ],
+    extensions: [
+      ".js",
+      ".vue",
+      ".json"
+    ],
+  },
+  server: {
+    port: 8080,
+  //   proxy: {
+  //     "^.*\.(json|componentdef)": {
+  //       "target": "http://localhost:8000",
+  //       changeOrigin: true,
+  //       "secure": false,
+  //       rewrite: path => {
+  //         console.log(path);
+  //         return path;
+  //       }
+  //     }
+  //   }
+  }
+});
