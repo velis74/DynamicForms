8784eaba457212af75f150474efaaf46422c6116
adamPrestor <adam.prestor@gmail.com>
2023-02-01 10:11:42 +0100

    #809 Create dynamicForms plugin with reference implementation into demo app

--- vue/main.ts
+++ vue/main.ts
@@ -1,43 +1,14 @@
 import { createApp } from 'vue';
-import { createVuetify, ThemeDefinition } from 'vuetify/dist/vuetify';
+
+import DemoApp from './demo-app';
+import dynamicForms from './plugins/dynamicForms';
+import vuetify from './plugins/vuetify';
+import router from './router';
 import 'vuetify/styles/main.css';
 
-import GenericColumn from './components/table/tcolumn-generic.vue';
-import * as VuetifyComponents from './components/vuetify';
-import DemoApp from './demo-app';
-import VuetifyViewMode from './demo-app/vuetify/view-mode.vue';
-import VuetifyApp from './demo-app/vuetify/vuetify-app.vue';
-import router from './router';
-
 const app = createApp(DemoApp);
-
-// vuetify components
-app.component(VuetifyApp.name, VuetifyApp);
-app.component(VuetifyViewMode.name, VuetifyViewMode);
-Object.values(VuetifyComponents).map((component) => app.component(component.name, component));
-
-app.component(GenericColumn.name, GenericColumn);
-
 app.use(router);
-
-const defaultTheme: ThemeDefinition = {
-  dark: false,
-  colors: {
-    background: '#f8f8f8',
-    surface: '#ffffff',
-    // 'primary-darken-1': '#3700B3',
-    // 'secondary-darken-1': '#018786',
-    primary: '#3f51b5',
-    secondary: '#2196f3',
-    accent: '#ffc107',
-    error: '#f44336',
-    warning: '#ff9800',
-    info: '#8bc34a',
-    success: '#00bcd4',
-  },
-};
-
-const vuetify = createVuetify({ theme: { defaultTheme: 'defaultTheme', themes: { defaultTheme } } });
 app.use(vuetify);
+app.use(dynamicForms);
 
 app.mount('#app');
---
+++ vue/plugins/dynamicForms.ts
@@ -0,0 +1,9 @@
+import createDynamicForms from "../dynamicforms";
+
+import type { DynamicFormsOptions } from "../dynamicforms"
+
+const options: DynamicFormsOptions = {
+  ui: 'vuetify'
+}
+
+export default createDynamicForms(options);
