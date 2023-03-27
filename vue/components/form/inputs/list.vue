74f01ddff7c519ce1fcaaca181d5e5e8ca289b82
adamPrestor <adam.prestor@gmail.com>
2023-03-27 13:33:12 +0200

    Create reminders table through api consumer

--- vue/components/form/inputs/list.vue
+++ vue/components/form/inputs/list.vue
@@ -5,10 +5,9 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 
+import APIConsumerLogic from '../../api_consumer/api-consumer-logic';
 import APIConsumer from '../../api_consumer/api-consumer.vue';
 import ComponentDisplay from '../../api_consumer/component-display';
-import ConsumerLogic from '../../api_consumer/consumer-logic';
-import apiClient from '../../util/api-client';
 import TranslationsMixin from '../../util/translations-mixin';
 
 import InputBase from './base';
@@ -18,25 +17,19 @@ export default defineComponent({
   components: { APIConsumer },
   mixins: [InputBase, TranslationsMixin],
   data() {
-    return { consumer: null as ConsumerLogic | null };
+    return { consumer: null as APIConsumerLogic | null };
   },
   computed: {
     displayComponent() {
       return ComponentDisplay.TABLE;
     },
   },
-  beforeMount() {
-    if (this.modelValue == null) {
-      // when creating new records we might get undefined, this breaks the reactivity
-      // update value with an empty array of records to avoid further complications
-      this.$emit('update:modelValue', []);
-    }
-  },
-  async mounted() {
-    const definition = (await apiClient.get(
-      `${this.field.renderParams.formComponentDef?.detail_url.split('/').slice(0, -1).join('/')}.componentdef`,
-    )).data;
-    this.consumer = new ConsumerLogic(definition, this.modelValue as any[]);
+  mounted() {
+    this.consumer = new APIConsumerLogic(
+      `${this.field.renderParams.formComponentDef?.detail_url.split('/').slice(0, -1).join('/')}`,
+    );
+    this.consumer.filter({ event: 1 });
+    this.consumer.getFullDefinition();
   },
 });
 </script>
