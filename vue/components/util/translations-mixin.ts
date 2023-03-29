dcb39555ba8da742afc1939c19b41e707f9c1839
Jure Erznožnik <jure@velis.si>
2023-03-29 15:17:24 +0200

    Support for translation function interpolate

--- vue/components/util/translations-mixin.ts
+++ vue/components/util/translations-mixin.ts
@@ -4,13 +4,25 @@
 declare global {
   interface Window {
     gettext?: (str: string) => string;
+    interpolate?: (str: string, data: any[] | { [key: string]: any }, named: boolean) => string;
   }
 }
 
-function gettext(str: string) {
+function gettext(str: string): string {
   return window.gettext ? window.gettext(str) : str;
 }
 
-export { gettext };
+function interpolate(str: string, data: { [key: string]: any }): string {
+  return window.interpolate ?
+    window.interpolate(str, data, true) :
+    str.replace(/%\(\w+\)s/g, (match) => String(data[match.slice(2, -2)]));
+}
 
-export default { methods: { gettext(str: string) { return gettext(str); } } };
+export { gettext, interpolate };
+
+export default {
+  methods: {
+    gettext(str: string) { return gettext(str); },
+    interpolate(str: string, data: { [key: string]: any }) { return interpolate(str, data); },
+  },
+};
