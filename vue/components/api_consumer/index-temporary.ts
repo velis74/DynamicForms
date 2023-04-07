4952c5e43ed64404a65b1f0e8b89d4d8640a78e7
Jure Erznožnik <jure@velis.si>
2023-04-07 10:20:46 +0200

    fix linter

--- vue/components/api_consumer/index-temporary.ts
+++ vue/components/api_consumer/index-temporary.ts
@@ -1,6 +1,6 @@
 import APIConsumerLoader from './api-consumer-loader.vue';
-import ConsumerLogicApi from './consumer-logic-api';
 import APIConsumer from './api-consumer.vue';
 import ComponentDisplay from './component-display';
+import ConsumerLogicApi from './consumer-logic-api';
 
 export { ComponentDisplay, APIConsumer, APIConsumerLoader, ConsumerLogicApi };
