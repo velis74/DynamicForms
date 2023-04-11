7f57555a95fc11aeb83a78769f4ee2a74e43a278
adamPrestor <adam.prestor@gmail.com>
2023-04-11 16:00:08 +0200

    Fix failing test cases

--- tests/unit/test_filter.py
+++ tests/unit/test_filter.py
@@ -1,15 +1,37 @@
+import json
+
 from django.urls import reverse
+from jsonschema import validate
 from rest_framework import status
 from rest_framework.test import APITestCase
 
+schema = {
+    "type": "array",
+    "items": {
+        "type": "object",
+        "properties": {
+            "id": {"type": "number"},
+            "df_control_data": {"type": "object"},
+            "df_prev_id": {"type": "string"},
+            "row_css_style": {"type": "string"},
+            "name": {"type": ["null", "string"]},
+            "rtf_field": {"type": ["null", "string"]},
+            "datetime_field": {"type": "string"},
+            "char_field": {"type": "string"},
+            "int_field": {"type": "number"},
+            "int_choice_field": {"type": "number"},
+            "bool_field": {"type": "boolean"},
+        },
+        "additionalProperties": False,
+    },
+}
+
 
 class FilterTest(APITestCase):
-
     def test_contains_filter(self):
-        response = self.client.get(reverse('filter-list', args=['html']))
+        response = self.client.get(reverse("filter-list", args=["json"]))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response['content-type'], 'text/html; charset=utf-8')
-        content = response.content.decode('utf-8')
-        self.assertTrue(
-            all(x in content for x in ['<html', '<body', '<table', '<tbody', '<tr class="dynamicforms-filterrow"',
-                                       'dynamicforms.defaultFilter(event)']))
+        self.assertEqual(response["content-type"], "application/json")
+        content = json.loads(response.content)
+        print(json.dumps(content, indent=4))
+        validate(content, schema)
