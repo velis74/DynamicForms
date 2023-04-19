964f852ada20be1777c091a94c850aa7b15c55d6
Uros Tement <ut@scantron.si>
2023-04-19 10:41:07 +0200

    Added support for .env files

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { resolve } from 'path';
 
 import vue from '@vitejs/plugin-vue';
 import { createProxyMiddleware } from 'http-proxy-middleware';
-import { defineConfig } from 'vite';
+import { ConfigEnv, defineConfig, loadEnv } from 'vite';
 import eslint from 'vite-plugin-eslint';
 import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify';
 
@@ -17,7 +17,7 @@ const axiosRedirectConfig = () => ({
     server.middlewares.use(
       '/',
       createProxyMiddleware(filter, {
-        target: 'http://localhost:8000',
+        target: process.env.VITE_AXIOS_TARGET,
         changeOrigin: false,
         pathRewrite: (path) => {
           // console.log('path', path);
@@ -28,63 +28,65 @@ const axiosRedirectConfig = () => ({
   },
 });
 
-/** @type {import('vite').UserConfig} */
-export default defineConfig({
-  plugins: [
-    vue(),
-    {
-      ...eslint({
-        failOnWarning: false,
-        failOnError: false,
-      }),
-      apply: 'serve',
-      enforce: 'post',
-    },
-    vuetify({ autoImport: true }),
-    axiosRedirectConfig(),
-  ],
-  resolve: {
-    // alias: {
-    //   '@': fileURLToPath(new URL_('./src', import.meta.url)),
-    //   '~': fileURLToPath(new URL_('./node_modules', import.meta.url)),
-    // },
-    extensions: [
-      '.js',
-      '.ts',
-      '.vue',
-      '.json',
-      '.css',
+export default ({mode}: ConfigEnv) => {
+  process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+  return defineConfig({
+    plugins: [
+      vue(),
+      {
+        ...eslint({
+          failOnWarning: false,
+          failOnError: false,
+        }),
+        apply: 'serve',
+        enforce: 'post',
+      },
+      vuetify({autoImport: true}),
+      axiosRedirectConfig(),
     ],
-  },
-  server: {
-    port: 8080,
-    fs: {
-      // Allow serving files from one level up to the project root
-      allow: ['..'],
+    resolve: {
+      // alias: {
+      //   '@': fileURLToPath(new URL_('./src', import.meta.url)),
+      //   '~': fileURLToPath(new URL_('./node_modules', import.meta.url)),
+      // },
+      extensions: [
+        '.js',
+        '.ts',
+        '.vue',
+        '.json',
+        '.css',
+      ],
     },
-  },
-  build: {
-    target: 'es2022',
-    lib: {
-      entry: resolve(__dirname, './vue/dynamicforms.ts'),
-      formats: ['umd'],
-      fileName: 'dynamicforms',
-      name: 'dynamicforms.[name]',
+    server: {
+      port: 8080,
+      fs: {
+        // Allow serving files from one level up to the project root
+        allow: ['..'],
+      },
     },
-    rollupOptions: {
-      external: ['axios', 'bootstrap', 'bootstrap-vue', 'lodash', 'vue', 'vue-ionicon', 'vue-router', 'vuetify'],
-      output: {
-        sourcemap: true,
-        globals: (id: string) => id, // all external modules are currently not aliased to anything but their own names
+    build: {
+      target: 'es2022',
+      lib: {
+        entry: resolve(__dirname, './vue/dynamicforms.ts'),
+        formats: ['umd'],
+        fileName: 'dynamicforms',
+        name: 'dynamicforms.[name]',
+      },
+      rollupOptions: {
+        external: ['axios', 'bootstrap', 'bootstrap-vue', 'lodash', 'vue', 'vue-ionicon', 'vue-router', 'vuetify'],
+        output: {
+          sourcemap: true,
+          globals: (id: string) => id, // all external modules are currently not aliased to anything but their own names
+        }
       }
-    }
-  },
-  test: {
-    deps: {
-      inline: ['vuetify']
     },
-    globals: true,
-    environment: 'jsdom',
-    useAtomics: true, // eliminates tests hang at the end (https://github.com/vitest-dev/vitest/issues/2008)
-  },
-});
+    test: {
+      deps: {
+        inline: ['vuetify']
+      },
+      globals: true,
+      environment: 'jsdom',
+      useAtomics: true, // eliminates tests hang at the end (https://github.com/vitest-dev/vitest/issues/2008)
+    },
+  });
+};
