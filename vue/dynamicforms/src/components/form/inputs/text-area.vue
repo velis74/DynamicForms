d03e9a7e28d3813a98b1176588128d03d6181c2b
adamPrestor <adam.prestor@gmail.com>
2023-05-09 14:59:57 +0200

    Refactor text-area (#174)

--- vue/dynamicforms/src/components/form/inputs/text-area.vue
+++ vue/dynamicforms/src/components/form/inputs/text-area.vue
@@ -1,49 +1,51 @@
 <template>
-  <InputBase :def="def" :data="data" :errors="errors" :show-label-or-help-text="showLabelOrHelpText">
-    <input
-      :id="def.uuid"
-      slot="input"
-      v-model="value"
-      :type="inputType"
-      :class="def.render_params.field_class"
-      :name="def.name"
-      :aria-describedby="def.help_text && showLabelOrHelpText ? def.name + '-help' : null"
-      :placeholder="def.placeholder"
+  <vuetify-input
+    :label="baseBinds.label"
+    :messages="baseBinds.messages"
+    :error-messages="baseBinds['error-messages']"
+    :error-count="baseBinds['error-count']"
+  >
+    <div style="width: 100%;">
+      <v-textarea
+        :id="field.uuid"
+        v-model="value"
+        :type="inputType"
+        variant="underlined"
+        :class="field.renderParams.fieldCSSClass"
+        :name="field.name"
+        :placeholder="field.placeholder"
 
-      :pattern="def.render_params.pattern"
-      :min="def.render_params.min"
-      :max="def.render_params.max"
-      :step="def.render_params.step"
-      :minlength="def.render_params.min_length"
-      :maxlength="maxLength"
-      :size="def.render_params.size"
+        :minlength="minLength"
+        :maxlength="maxLength"
+        :step="field.renderParams.step"
+        :size="field.renderParams.size"
 
-      :readonly="def.read_only === true"
-      :disabled="def.read_only === true"
-    >
-  </InputBase>
+        :readonly="field.readOnly"
+        :disabled="field.readOnly"
+      />
+    </div>
+  </vuetify-input>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 
+import TranslationsMixin from '../../util/translations-mixin';
+
 import InputBase from './base';
+import VuetifyInput from './input-vuetify.vue';
 
 export default /* #__PURE__ */ defineComponent({
   name: 'DTextArea',
-  components: { InputBase },
-  props: {
-    def: { type: Object, required: true },
-    data: { type: Object, required: true },
-    errors: { type: Object, required: true },
-    showLabelOrHelpText: { type: Boolean, default: true },
-  },
+  components: { VuetifyInput },
+  mixins: [InputBase, TranslationsMixin],
   computed: {
-    inputType() { return this.def.render_params.input_type; },
-    maxLength() { return this.def.render_params.max_length || (1 << 24); }, // eslint-disable-line no-bitwise
+    inputType() { return this.field.renderParams.inputType; },
+    minLength() { return this.field.renderParams.minLength; },
+    maxLength() { return this.field.renderParams.maxLength; },
     value: {
-      get: function get() { return this.data[this.def.name]; },
-      set: function set(newVal) { this.data[this.def.name] = newVal; } // eslint-disable-line
+      get: function get() { return this.modelValue; },
+      set: function set(newVal: string) { this.$emit('update:modelValue', newVal); },
     },
   },
 });
