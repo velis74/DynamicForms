8aaa210fc5affb85f46bb1bda854f7a99de8e403
Jure Erznožnik <jure@velis.si>
2023-08-01 15:38:43 +0200

    fix related fields

--- dynamicforms/mixins/related_field_ajax.py
+++ dynamicforms/mixins/related_field_ajax.py
@@ -1,6 +1,8 @@
 from typing import Optional
 
 from django.urls import reverse
+from rest_framework.relations import ManyRelatedField
+from rest_framework.serializers import ListSerializer
 
 
 class RelatedFieldAJAXMixin(object):
@@ -84,3 +86,38 @@ class RelatedFieldAJAXMixin(object):
                 )
             )
         return res
+
+    # noinspection PyUnusedLocal, PyUnresolvedReferences
+    def render_to_table(self, value, row_data):
+        """
+        Renders field value for table view :if rendering to html table, let's try to resolve any lookups
+        hidden fields will render to tr data-field_name attributes, so we maybe want to have ids, not text there,
+          but that's up to front end to decide
+
+        :param value: field value
+        :param row_data: data for entire row (for more complex renderers)
+        :return: rendered value for table view
+        """
+        if isinstance(self, ManyRelatedField):
+            # Hm, not sure if this is the final thing to do: an example of this field is in
+            # ALC plane editor (modes of takeoff). However, value is a queryset here. There seem to still be DB queries
+            # However, in the example I have, the problem is solved by doing prefetch_related on the m2m relation
+            cr = self.child_relation
+            return ", ".join((cr.display_value(item) for item in value))
+            # return ', '.join((cr.display_value(item) for item in cr.get_queryset().filter(pk__in=value)))
+
+        return value
+
+    # noinspection PyUnresolvedReferences
+    def to_internal_value(self, data):
+        """
+        Reverse of to_representation: if data coming in is a tuple, use just the "id/code/key" part, not entire tuple
+        """
+        if (
+            self.field_name not in ("df_control_data", "df_prev_id", "row_css_style")
+            and not isinstance(self, ListSerializer)
+            and isinstance(data, list)
+        ):
+            data = data[0]
+
+        return super().to_internal_value(data)
