303649e22554da9052ad0cc0c0873a329c8fe0ee
adamPrestor <adam.prestor@gmail.com>
2023-09-19 11:03:14 +0200

    Refactor field into composition

--- vue/dynamicforms/src/components/form/field.vue
+++ vue/dynamicforms/src/components/form/field.vue
@@ -12,12 +12,11 @@
   </v-col>
 </template>
 
-<script lang="ts">
+<script setup lang="ts">
 import _ from 'lodash';
-import { defineComponent, inject } from 'vue';
+import { computed, inject, watch } from 'vue';
 
 import { useActionHandler } from '../actions/action-handler-composable';
-import ActionHandlerMixin from '../actions/action-handler-mixin';
 import FilteredActions from '../actions/filtered-actions';
 
 import FormField from './definitions/field';
@@ -32,67 +31,47 @@ import DPlaceholder from './inputs/placeholder.vue';
 import DSelect from './inputs/select.vue';
 import DTextArea from './inputs/text-area.vue';
 
-export default /* #__PURE__ */ defineComponent({
-  name: 'FormField',
-  components: {
-    DCheckbox,
-    DCKEditor,
-    DDateTime,
-    DFile,
-    DInput,
-    DList,
-    DPassword: DInput,
-    DPlaceholder,
-    DSelect,
-    DTextArea,
+interface Props {
+  field: FormField
+  actions: FilteredActions
+  errors: any
+  showLabelOrHelpText: boolean
+  cssClasses?: string
+}
+
+const props = withDefaults(defineProps<Props>(), { cssClasses: 'col' });
+
+const { callHandler } = useActionHandler();
+const payload = inject<FormPayload>('payload', {} as FormPayload);
+
+const columnClasses = computed<string>(() => {
+  const classes = props.field.widthClasses;
+  return classes ? ` ${classes}` : '';
+});
+
+const fieldValue = computed({
+  get(): any { return payload[props.field.name]; },
+  set(value: any) {
+    payload[props.field.name] = value;
   },
-  mixins: [ActionHandlerMixin],
-  props: {
-    field: { type: FormField, required: true },
-    actions: { type: FilteredActions, required: true },
-    errors: { type: Object, required: true },
-    showLabelOrHelpText: { type: Boolean, default: true },
-    cssClasses: { type: String, default: 'col' },
-  },
-  setup() {
-    const { callHandler } = useActionHandler();
-    return { callHandler, payload: inject<FormPayload>('payload', {} as FormPayload) };
-  },
-  computed: {
-    columnClasses(): string {
-      const classes = this.field.widthClasses;
-      return classes ? ` ${classes}` : '';
-    },
-    fieldValue: {
-      get(): any { return this.payload[this.field.name]; },
-      set(value: any) {
-        this.payload[this.field.name] = value;
-      },
-    },
-  },
-  watch: {
-    fieldValue: {
-      handler(newValue: any, oldValue: any) {
-        this.debounceHandler(this, newValue, oldValue);
-      },
-      // in case there are nested values in the future
-      deep: true,
-    },
-  },
-  methods: {
-    updateModelValueDisplay(newValue: any) {
-      const fieldName = `${this.field.name}-display`;
-      if (Object.getOwnPropertyDescriptor(this.payload, fieldName)?.writable) {
-        this.payload[fieldName] = newValue;
-      }
-    },
-    debounceHandler: _.debounce((self, newValue, oldValue) => {
-      self.callHandler(
-        self.actions.valueChanged,
-        { field: self.field.name, oldValue, newValue },
-      );
-    }, 600),
-  },
+});
+
+const debounceHandler = _.debounce((newValue: any, oldValue: any) => {
+  callHandler(
+    props.actions.valueChanged,
+    { field: props.field.name, oldValue, newValue }
+  );
+}, 600);
+
+const updateModelValueDisplay = (newValue: any) => {
+  const fieldName = `${props.field.name}-display`;
+  if (Object.getOwnPropertyDescriptor(payload, fieldName)?.writable) {
+    payload[fieldName] = newValue;
+  }
+};
+
+watch(fieldValue, (newValue: any, oldValue: any) => {
+  debounceHandler(newValue, oldValue);
 });
 </script>
 
