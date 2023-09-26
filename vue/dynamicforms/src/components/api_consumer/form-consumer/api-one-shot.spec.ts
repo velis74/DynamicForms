7d08ea762649c4bd61dcf73005bf978fec8eff35
adamPrestor <adam.prestor@gmail.com>
2023-09-26 08:57:26 +0200

    Add tests

---
+++ vue/dynamicforms/src/components/api_consumer/form-consumer/api-one-shot.spec.ts
@@ -0,0 +1,17 @@
+import MockAdapter from 'axios-mock-adapter';
+
+import apiClient from '../../util/api-client';
+
+import FormConsumerApiOneShot from './api-one-shot';
+import data from './api-one-shot.spec.json';
+
+const mock = new MockAdapter(apiClient);
+
+describe('Api One Shot', () => {
+  it('Create', () => {
+    mock.onGet('/test.componentdef').reply(200, data);
+
+    const consumer = FormConsumerApiOneShot({ url: '/test' });
+    expectTypeOf(consumer).toEqualTypeOf(FormConsumerApiOneShot);
+  });
+});
