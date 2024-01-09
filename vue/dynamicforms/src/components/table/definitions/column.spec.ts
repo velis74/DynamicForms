d0db106907ce120fb46087dd38341535f4263951
Doroteja <doroteja.minjovic@gmail.com>
2024-01-09 08:21:02 +0100

    Changed .spec.ts file
    Removed old code from column.ts

--- vue/dynamicforms/src/components/table/definitions/column.spec.ts
+++ vue/dynamicforms/src/components/table/definitions/column.spec.ts
@@ -115,8 +115,8 @@ describe('TableColumn', () => {
     };
 
     const column = new TableColumn(initialData, orderingArray);
-    expect(column.renderDecoratorFunction(rowData, thead)).toBe('<code>false</code>');
-    expect(column.renderDecoratorFunction(rowData, !thead)).toBe(rowData.enabled);
+    expect(column.renderDecoratorFunction(rowData, thead)).toBe('<span style="color: red;">&#10008;</span>');
+    expect(column.renderDecoratorFunction(rowData, !thead)).toBe('<span style="color: green;">&#10004;</span>');
   });
   test('renderDecoratorEmail should return the expected string', () => {
     const initialData: ColumnJSON = {
