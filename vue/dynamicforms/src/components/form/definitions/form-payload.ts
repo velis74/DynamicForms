489b4cce9a49d2d9666cf675c6be56490b507fc4
Adam Prestor <adam.prestor@gmail.com>
2024-01-29 13:10:55 +0100

    Fix losing reactivity on consumer logic data (#243)
    * Fix losing reactivity on consumer logic data
    
    * Fix test, make FormPayload configurable

--- vue/dynamicforms/src/components/form/definitions/form-payload.ts
+++ vue/dynamicforms/src/components/form/definitions/form-payload.ts
@@ -15,6 +15,21 @@ export default class FormPayload {
   constructor(data: FormPayload);
   constructor(data: APIConsumer.FormPayloadJSON, layout: FormLayout);
   constructor(data?: APIConsumer.FormPayloadJSON, layout?: FormLayout) {
+    this.setData(data, layout);
+  }
+
+  addExtraData(data: { [key: string]: any }) {
+    const extraData = { ...this['$extra-data'], ...data };
+    Object.defineProperty(this, '$extra-data', { get() { return extraData; }, enumerable: false, configurable: true });
+  }
+
+  clear() {
+    Object.getOwnPropertyNames(this).forEach((key) => {
+      delete this[key];
+    });
+  }
+
+  setData(data?: APIConsumer.FormPayloadJSON, layout?: FormLayout): FormPayload {
     let properties = {} as { [key: string]: any };
     let extraData = {};
 
@@ -27,25 +42,28 @@ export default class FormPayload {
       Object.values(layout.fields).forEach((field: FormField) => {
         if (field.visibility === DisplayMode.SUPPRESS) return;
         if (field.readOnly && !field.name.endsWith('-display')) {
-          properties[field.name] = { get() { return data[field.name]; }, enumerable: false };
+          properties[field.name] = { get() { return data[field.name]; }, enumerable: false, configurable: true };
         } else {
           const self = this;
           self[field.name] = data[field.name];
           Object.defineProperty(self, `set${field.name}Value`, {
             get() { return function setValue(newValue: any) { self[field.name] = newValue; }; },
             enumerable: false,
+            configurable: true,
           });
         }
       });
     }
     Object.defineProperty(this, '$extra-data', { get() { return extraData; }, enumerable: false, configurable: true });
     Object.defineProperties(this, properties);
-    Object.defineProperty(this, '_properties', { get() { return properties; }, enumerable: false });
+    Object.defineProperty(this, '_properties', { get() { return properties; }, enumerable: false, configurable: true });
+
+    return this;
   }
 
-  addExtraData(data: { [key: string]: any }) {
-    const extraData = { ...this['$extra-data'], ...data };
-    Object.defineProperty(this, '$extra-data', { get() { return extraData; }, enumerable: false, configurable: true });
+  replaceData(data?: APIConsumer.FormPayloadJSON, layout?: FormLayout): FormPayload {
+    this.clear();
+    return this.setData(data, layout);
   }
 
   private copyWithProperties(base: FormPayload): [_properties: any, extraData: any] {
