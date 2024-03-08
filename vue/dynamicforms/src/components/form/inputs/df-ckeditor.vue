ebf2c606dab31111034e3d92874f2aecc6bc9528
adamPrestor <adam.prestor@gmail.com>
2024-03-08 16:11:16 +0100

    Keep the latest data in form consumer upon api error

--- vue/dynamicforms/src/components/form/inputs/df-ckeditor.vue
+++ vue/dynamicforms/src/components/form/inputs/df-ckeditor.vue
@@ -5,13 +5,14 @@
     :error-messages="baseBinds['error-messages']"
     :error-count="baseBinds['error-count']"
   >
-    <ckeditor v-model="value" :editor="editor" :config="editorConfig"/>
+    <ckeditor ref="$editor" v-model="value" :editor="editor" :config="editorConfig"/>
   </vuetify-input>
 </template>
 
 <script setup lang="ts">
 import '@velis/ckeditor-dist/styles.css';
 import Editor from '@velis/ckeditor-dist';
+import { computed, ref } from 'vue';
 
 import { BaseEmits, BaseProps, basePropsDefault, useInputBase } from './base';
 import VuetifyInput from './input-vuetify.vue';
@@ -24,8 +25,11 @@ const emits = defineEmits<Emits>();
 
 const { baseBinds, value } = useInputBase(props, emits);
 
+const $editor = ref(null);
+
 // data
 const editor = Editor;
-const editorConfig = {}; // The configuration of the editor.
+// The configuration of the editor.
+const editorConfig = computed(() => ({}));
 
 </script>
