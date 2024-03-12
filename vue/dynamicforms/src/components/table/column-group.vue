4bfe34c94f035c4856063373b5d7cad89bae2f30
Adam Prestor <adam.prestor@gmail.com>
2024-03-12 15:46:36 +0100

    Enable column filters on column groups (#259)

--- vue/dynamicforms/src/components/table/column-group.vue
+++ vue/dynamicforms/src/components/table/column-group.vue
@@ -7,6 +7,7 @@
         :column="field"
         :row-data="rowData"
         :actions="actions"
+        :filter-definition="filterDefinition"
       />
     </div>
   </div>
@@ -15,12 +16,14 @@
 <script setup lang="ts">
 import FilteredActions from '../actions/filtered-actions';
 
+import TableFilterRow from './definitions/filterrow';
 import { ColumnGroup } from './definitions/responsive-layout';
 
 defineProps<{
   column: ColumnGroup,
   rowData: Record<string, any>,
   actions: FilteredActions,
+  filterDefinition?: TableFilterRow,
 }>();
 </script>
 <script lang="ts">
