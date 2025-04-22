ddc02d9cc0b90e61f67df4404eb808e180887a6e
Jure Erznožnik <jure@velis.si>
2025-04-22 09:41:56 +0200

    - fix luxon to date-fnt issue - fix vite 5 -> 6 issue

--- vue/demo/src/main.ts
+++ vue/demo/src/main.ts
@@ -9,7 +9,7 @@ import router from './router';
 import 'vuetify/styles/main.css';
 import '@mdi/font/css/materialdesignicons.css';
 // eslint-disable-next-line import/no-relative-packages
-import '../../dynamicforms/dist/style.css';
+import '../../dynamicforms/dist/dynamicforms.css';
 import '@dynamicforms/vuetify-inputs/styles.css';
 
 const app = createApp(DemoApp);
--- vue/dynamicforms/src/components/table/cell-renderers/table-cell-date-time.vue
+++ vue/dynamicforms/src/components/table/cell-renderers/table-cell-date-time.vue
@@ -23,6 +23,10 @@ const format = computed(() => props.column.renderParams.table_format || 'dd.MM.y
 const displayValue = computed(() => {
   if (props.thead) return value.value;
   if (value.value == null) return '';
-  return formatDate(parseISO(value.value), format.value);
+  try {
+    return formatDate(parseISO(value.value), format.value);
+  } catch (err) {
+    return '';
+  }
 });
 </script>
